740aa2cf7191f85929d439c16af192a5d551fbcc
chernanat <chernan1234@hotmail.com>
2023-06-29 15:41:59 -0500

    mysql module added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const mysql = require("mysql");
 const { PORT } = require("./config");
 const inventarioRoutes = require("./routes/Inventario.routes");
 const app = express();
@@ -7,6 +8,13 @@ const app = express();
 app.use(morgan("dev"));
 
 app.use(inventarioRoutes);
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "inventario",
+});
 
 app.listen(PORT, () => {
   console.log(`ESCUCHANDO EN EL PUERTO : ${PORT}`);
