29d6176f2d05783b3717611061a845c2d95291f5
chernanat <chernan1234@hotmail.com>
2023-06-29 22:00:19 -0500

    ejs module added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 const mysql = require("mysql");
 const { PORT } = require("./config");
 const inventarioRoutes = require("./routes/Inventario.routes");
 const app = express();
 
+//settings
 app.use(morgan("dev"));
-
+app.use(express.urlencoded({ extended: false }));
 app.use(inventarioRoutes);
+app.set("view engine", "ejs");
+app.set("/views", path.join(__dirname, "/views"));
 
+//db connection
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
