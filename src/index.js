538d5c2e6abcb06bf79086771272576521039678
chernanat <chernan1234@hotmail.com>
2023-07-02 21:15:48 -0500

    environment variables index

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const mysql = require("mysql");
-const { PORT } = require("./config");
+const { PORT, HOST, USER, PASSWORD, DB_PORT, DATABASE_NAME } = require("./config");
 const inventarioRoutes = require("./routes/Inventario.routes");
 const myConnection = require('express-myconnection');
 const app = express();
-// const DataTable = require('datatables.net-dt')
-// import DataTable from 'datatables.net-dt';
  
-// let table = new DataTable('#myTable');
 //settings
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -18,11 +15,11 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(morgan("dev"));
 //conexion a la db
 app.use(myConnection(mysql,{
-  host: "localhost",
-  user: "root",
-  password: "",
-  port: 3306,
-  database: "inventario",
+  host: HOST,
+  user: USER,
+  password: PASSWORD,
+  port: DB_PORT,
+  database: DATABASE_NAME,
 }, 'single'));
 
 app.use(express.urlencoded({ extended: false }));
